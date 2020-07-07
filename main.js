4a6254903a6e9632c265c493497afdd79c6d89ae
felixkan1 <felix.kan@mail.utoronto.ca>
2020-07-07 19:39:32 -0400

    added opacity change when scrolling

--- main.js
+++ main.js
@@ -3,16 +3,46 @@
 const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 const bar = document.querySelector(".hamburger .bar")
+const menuItems = document.querySelectorAll(".nav-list ul li a");
 
-let open = false;
+menuItems.forEach(element =>{
+  element.addEventListener("click",()=>{
+    navList.classList.toggle("active");
+    bar.classList.toggle("active");
+  })
+})
+
 hamburger.addEventListener("click", ()=>{
-  open = !open;
-  if(open){
-    navList.classList.add("active");
-    bar.classList.add("active");
-  }else{
-    navList.classList.remove("active"); 
-    bar.classList.remove("active");
+    navList.classList.toggle("active");
+    bar.classList.toggle("active");
+}) 
+
+
+
+
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if(window.innerWidth > 1200){
+      if (scroll_position > 250) {
+      document.querySelector(".nav-list").style.backgroundColor = '#4f4a4a';
+    } else {
+      document.querySelector(".nav-list").style.backgroundColor = 'transparent';
+    }
   }
   
-}) 
+});
+
+if(window.innerWidth < 1200){
+  document.querySelector(".nav-list").style.backgroundColor = '#4f4a4a';
+}
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
